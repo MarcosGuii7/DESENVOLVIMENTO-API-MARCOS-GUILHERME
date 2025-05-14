0348fd00e2627cd4f797280f45cd13136d5e10c1
MarcosGuii7 <contato.marcosguilherme.dsg@gmail.com>
2025-05-14 15:52:14 -0300

    Add files via upload

---
+++ celular.js
@@ -0,0 +1,28 @@
+let celular = {
+    marca: "Samsung",
+    modelo: "A23 5G",
+    cor: "Azul",
+    processador: " Adreno 619",
+    bateria: "5000 mAh"
+} 
+
+let celular2 = {
+    marca: "Apple",
+    modelo: "Iphone 15",
+    cor: "Preto",
+    processador: "Apple A16 Bionic (4 nm)",
+    bateria: "3.349 mAh"
+}
+
+let celular3 = {
+    marca: "Samsung",
+    modelo: "S25 5G",
+    cor: "Cinza",
+    processador: "Snapdragon 8 Elite for Galaxy Qualcomm SM8750-AB",
+    bateria: "4000 mAh"
+}
+
+let objetos = [celular, celular2, celular3]
+
+console.log(objetos[1].processador)
+console.log(objetos[0].modelo)
---
+++ objeto.js
@@ -0,0 +1,8 @@
+let usuario = {
+    nome: 'Marcos',
+    idade: 17,
+    email: "marcosguilhermegui018@gmail.com"
+}
+
+console.log(usuario.nome)
+console.log(usuario.idade)
